77b804244fab0ab5f2f7f82acf64ccb712becc6b
Miguel Correa <miguel_correa12@hotmail.com>
2023-11-07 22:23:55 +0000

    bug fix

--- src/MiniProj1/src/main.ts
+++ src/MiniProj1/src/main.ts
@@ -4,13 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
-
-library.add(faInstagram)
-
 const app = createApp(App)
-
 
 app.use(router)
 
